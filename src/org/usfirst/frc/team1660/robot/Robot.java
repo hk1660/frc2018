8d07c7305738d746d22de2585571b9316c3cd2b7
jabia <jabia@DESKTOP-V2LSDH4>
2018-01-18 00:12:44 -0500

    Added Python Vision folder with starter python code Chris posted (using pyDev plugin for Eclipse)

--- src/org/usfirst/frc/team1660/robot/Robot.java
+++ src/org/usfirst/frc/team1660/robot/Robot.java
@@ -4,39 +4,51 @@
 
 package org.usfirst.frc.team1660.robot;
 
+/*-----IMPORTED LIBRARIES-----*/
 
-import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
-import com.kauailabs.navx.frc.AHRS;
-import edu.wpi.first.wpilibj.DriverStation;
-import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.IterativeRobot;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
-import edu.wpi.first.wpilibj.SPI;
+import edu.wpi.first.wpilibj.DriverStation;
+import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.drive.MecanumDrive;
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
+
+import com.kauailabs.navx.frc.AHRS;
+import edu.wpi.first.wpilibj.SPI;
+
 
 public class Robot<m_robotDrive> extends IterativeRobot {
 
 	/*----DECLARED GLOBAL VARIABLES-------*/
+	
+	//Drivetrain Declarations
 	private static final int kFrontLeftChannel = 2;
 	private static final int kBackLeftChannel = 3;
 	private static final int kFrontRightChannel = 1;
 	private static final int kBackRightChannel = 0;
-	private static final int kSwitchBoi = 5; 			//lol we can switch the name l8r
-	private static final int kClimbMotorChannel = 4;
-	private static final int kJoystickChannel = 0;
-
-	private MecanumDrive m_robotDrive;
-	private Joystick m_stick1;
+	WPI_TalonSRX frontLeft;
+	WPI_TalonSRX backLeft;
+	WPI_TalonSRX frontRight;
+	WPI_TalonSRX backRight;
+	private MecanumDrive mecDrive;
+	
 	private AHRS navx;
-
-	WPI_TalonSRX climbMotor = new WPI_TalonSRX(kClimbMotorChannel);
-	WPI_TalonSRX spitMotor = new WPI_TalonSRX(5);
-
-
 	double roboAngle = 0.0;
 	double zeroedYawPoint = 0.0;
 
-	//JOYSTICK
+	
+	//Manipulator Declarations
+	private static final int kMouthMotorChannel = 4;
+	private static final int kLiftMotorChannel = 5;	
+	private static final int kSwitchBoi = 6; 			//lol we can switch the name l8r
+
+	WPI_TalonSRX liftMotor;
+	WPI_TalonSRX mouthMotor;
+
+
+	//JOYSTICKS
+	Joystick driverStick = new Joystick(0);
+	Joystick manipStick = new Joystick(1);
 	final int A_BUTTON = 1;
 	final int B_BUTTON = 2;
 	final int X_BUTTON = 3;
@@ -58,50 +70,39 @@ public class Robot<m_robotDrive> extends IterativeRobot {
 	final int POV_DOWN = 180;
 	final int POV_RIGHT = 90;
 
-	Joystick driverStick = new Joystick(0);
-	Joystick manipStick = new Joystick(1);
-	private Joystick m_stick;
+
 	
     public void robotInit() {
 
-    	 WPI_TalonSRX climbMotor = new WPI_TalonSRX(kClimbMotorChannel);
-    	 //A.K.A Climb Manipulator
-		WPI_TalonSRX frontLeft = new WPI_TalonSRX(kFrontLeftChannel);
-		WPI_TalonSRX backLeft = new WPI_TalonSRX(kBackLeftChannel);
-		WPI_TalonSRX frontRight = new WPI_TalonSRX(kFrontRightChannel);
-		WPI_TalonSRX backRight = new WPI_TalonSRX(kBackRightChannel);
+    	//Drivetrain Initializations
+		frontLeft = new WPI_TalonSRX(kFrontLeftChannel);
+		backLeft = new WPI_TalonSRX(kBackLeftChannel);
+		frontRight = new WPI_TalonSRX(kFrontRightChannel);
+		backRight = new WPI_TalonSRX(kBackRightChannel);
+		
+		frontLeft.setInverted(true);	// Invert the left side motors (Is this needed?)
+		backLeft.setInverted(true);		// Invert the left side motors (Is this needed?)
 
+		mecDrive = new MecanumDrive(frontLeft, backLeft, frontRight, backRight);
 
+		
+    	//Manipulator Initializations
+    	mouthMotor = new WPI_TalonSRX(kMouthMotorChannel);
+    	liftMotor = new WPI_TalonSRX(kLiftMotorChannel); //A.K.A Elevator/Climb Manipulator
+		
+		
+		//Sensor Intializations
 		try {
 			navx = new AHRS(SPI.Port.kMXP); //navX-MXP initialized with (SPI, I2C, TTL UART) and USB //http://navx-mxp.kauailabs.com/guidance/selecting-an-interface.
 		} catch (RuntimeException ex ) {
 			DriverStation.reportError("Error instantiating navX MXP:  " + ex.getMessage(), true);
 		}
 
-		// Invert the left side motors.
-		// You may need to change or remove this to match your robot.
-		frontLeft.setInverted(true);
-		backLeft.setInverted(true);
 
-		m_robotDrive = new MecanumDrive(frontLeft, backLeft, frontRight, backRight);
-
-		m_stick1 = new Joystick(kJoystickChannel);
-		
-		
 	}
 
 
-	
-	
-	public void climbDown1() {
-	climbMotor.set(-1.0);
-		
-	}
-
-
-
-
-	/* AUTONOMOUS MODE */
+	/*----- AUTONOMOUS MODE -----*/
 	public void autonomousInit() {
 		//autocode goes here @AmadouGamby & @marlahna
 		// timer.reset(); // Resets the timer to 0
@@ -111,24 +112,25 @@ public class Robot<m_robotDrive> extends IterativeRobot {
 
 	}
 
-	/* TELEOP MODE */ 
+	/*----- TELEOP MODE -----*/ 
 	public void teleopInit() { 
-		
 		
 	}
 	public void teleopPeriodic() {
 		// Use the joystick X axis for lateral movement, Y axis for forward
 		// movement, and Z axis for rotation.
-		m_robotDrive.driveCartesian(m_stick1.getX(), m_stick1.getY(),
-				m_stick1.getZ(), 0.0);
+		mecDrive.driveCartesian(driverStick.getX(), driverStick.getY(), driverStick.getZ(), 0.0);
+		
+		getCurrentAngle();
+
 	}
 
 
 
 
+	/*----- CUSTOM METHODS -----*/
 
-	/* CUSTOM METHODS */
-
+	// Method to calculate current angle of robot based off of the field -Aldenis
 	public int getCurrentAngle(){
 		int moddedAngle = Math.floorMod((int)navx.getAngle(), 360);
 		int moddedZeroedYawPoint = Math.floorMod((int)zeroedYawPoint, 360);
@@ -138,42 +140,34 @@ public class Robot<m_robotDrive> extends IterativeRobot {
 		return modAngle;
 	}
 	
-	// Aldenis
-	
-
-	
-	
-	
-	//Robot Climbs down -Aldenis 
+	//Basic method to climb down -Aldenis
 	public void climbDown() {
-		climbMotor.set(-1.0);
+		liftMotor.set(-1.0);
 	}
-
 	
-	// Robot spits out a PowerCube -Kwaku Boafo
+	
+	// Basic method for robot to spit out a PowerCube -Kwaku Boafo
 	public void spit(){
-		spitMotor.set(1.0);
+		mouthMotor.set(1.0);
 	}
 
-
-
-
-
 	
-	
-	
-	//method to get the value from the armabot encoder- lakiera and pinzon
+	//method to get the value from the Lift encoder- lakiera and pinzon
 	public int getEncoder(){
 		return 0;
-		
-		
-		
-		
 	}
+
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 	
 }
-	
-	
-	
-
-
