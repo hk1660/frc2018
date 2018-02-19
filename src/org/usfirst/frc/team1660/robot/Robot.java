df9394a47d88dc0b5a8e95d96de356420b8eadd9
jabia <jabia@DESKTOP-V2LSDH4>
2018-02-19 00:35:34 -0500

    eliminated unused code

--- src/org/usfirst/frc/team1660/robot/Robot.java
+++ src/org/usfirst/frc/team1660/robot/Robot.java
@@ -6,19 +6,15 @@
 
 package org.usfirst.frc.team1660.robot;
 
-import com.ctre.phoenix.motorcontrol.ControlMode;
-import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
-import edu.wpi.first.wpilibj.CameraServer;
-
-import edu.wpi.first.wpilibj.DriverStation;
 
 /*-----IMPORTED LIBRARIES-----*/
-
 import edu.wpi.first.wpilibj.IterativeRobot;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj.DriverStation;
+import edu.wpi.first.wpilibj.CameraServer;
 
 
 public class Robot<m_robotDrive> extends IterativeRobot {
@@ -28,8 +24,6 @@ public class Robot<m_robotDrive> extends IterativeRobot {
 	HKDrive hkdrive = new HKDrive(driverStick);
 	Lift liftMani = new Lift(maniStick);
 	Mouth mouthMani = new Mouth(maniStick);
-	//Lidar laser = new Lidar();
-	//Lidar2 laser2 = new Lidar2();
 	Lidar3 laser3 = new Lidar3();
 	@SuppressWarnings("rawtypes")
 	SendableChooser strategy = new SendableChooser();
@@ -68,16 +62,16 @@ public class Robot<m_robotDrive> extends IterativeRobot {
 	public void autonomousInit() {
 
 		// Auto mode strategies setup
-		strategy.addDefault("justCrossAutoLineStrategy", new Integer(1));
-		strategy.addObject("simpleSwitchStrategy", new Integer(2));
-		strategy.addObject("smartSwitchStrategy", new Integer(3));
-		strategy.addObject("smartSwitchLidarStrategy", new Integer(4));
-		strategy.addObject("simpleScaleStrategy", new Integer(5));
+		strategy.addDefault("justCrossAutoLineStrategy(1)", new Integer(1));
+		strategy.addObject("simpleSwitchStrategy(2)", new Integer(2));
+		strategy.addObject("smartSwitchStrategy(3)", new Integer(3));
+		strategy.addObject("smartSwitchLidarStrategy(4)", new Integer(4));
+		strategy.addObject("simpleScaleStrategy(5)", new Integer(5));
 		SmartDashboard.putData("strategy selector", strategy); 
 
-		position.addDefault("Left", new Integer(1));
-		position.addObject("Middle", new Integer(2));
-		position.addObject("Right", new Integer(3));
+		position.addDefault("Left(1)", new Integer(1));
+		position.addObject("Middle(2)", new Integer(2));
+		position.addObject("Right(3)", new Integer(3));
 		SmartDashboard.putData("position selector", position); 
 
 		timerAuto.start();
@@ -95,14 +89,10 @@ public class Robot<m_robotDrive> extends IterativeRobot {
 		newTravelFlag = true;
 		this.mouthMani.shutUp();
 
-		//  hkdrive.setSafetyEnabled(false);
-
 	}
 
 	//AUTO_PERIODIC
 	public void autonomousPeriodic() {
-
-		//while(isAutonomous() && isEnabled()){ 
 
 		//get current Auto time for SmartDash
 		double autoTime = timerAuto.get();
@@ -116,13 +106,13 @@ public class Robot<m_robotDrive> extends IterativeRobot {
 		if(currentStrategy == 1) { 
 			this.justCrossAutolineStrategy(autoTime);
 		} else if (currentStrategy == 2) {
-			this.simpleSwitchStrategy(autoTime, currentPosition);
+			this.simpleSwitchStrategy(autoTime);
 		} else if (currentStrategy == 3) {
 			this.smartSwitchStrategy(autoTime);
 		} else if (currentStrategy == 4) {
 			this.smartSwitchLidarStrategy(autoTime);
 		} else if (currentStrategy == 5) {
-			this.simpleScaleStrategy(autoTime, currentPosition);
+			this.simpleScaleStrategy(autoTime);
 		}
 
 	}
@@ -163,45 +153,57 @@ public class Robot<m_robotDrive> extends IterativeRobot {
 	//QUICK AUTO STRATEGY #1: this is to get to the auto line assuming position 1 or 3 -Marlahna
 	public void justCrossAutolineStrategy (double timeA) { 
 
-		//double forwardSpeed = 0.5;
 		double forwardVoltage = 6.0;
-		double turnVoltage = 8.0;
 
-
-		if(currentPosition == 3) {
-			turnVoltage *= -1;
-		}
-
-
-		//podiyion 1 needs a posi turn
-		//pos 3 needs a neg turn
-
-		double startPauseTime = .5;						//1.0	
-		double firstForwardTime = 2.75 + startPauseTime;			//2.0
-		double firstTurnTime = .625 +firstForwardTime;			//2.5
-		double secondForwardTime = .5 + firstTurnTime;			//3.5
-		double dipTime = 0.6 + secondForwardTime;				//7.0
-		double lastTime = 6.0;
+		double startPauseTime = .5;									//0.5	
+		double firstForwardTime = 2.75 + startPauseTime;			//3.25
 
 		if(timeA < startPauseTime){
 		}else if (timeA < firstForwardTime) {
 			hkdrive.goForwardVoltage(forwardVoltage);
 			liftMani.elevatorLift(liftMani.switchHeight);	
-		}else if(timeA < firstTurnTime) {
-			hkdrive.turnVoltage(turnVoltage);
-		}else if(timeA < secondForwardTime) {
+		}else{
+			hkdrive.stop();	
+		}
+	}
+
+
+	//AUTO STRATEGY #2: Starting from position 1 or 3, go forward to a particular plate of our switch, drop off the powercube if its the correct one -Marlahna
+	public void simpleSwitchStrategy(double timeB) {
+
+		double forwardVoltage = 6.0;
+		double turnVoltage = 8.0;
+
+		//position 3 needs a neg turn, position 1 needs a positive turn
+		if(currentPosition == 3) {
+			turnVoltage *= -1;
+		}
+
+		double startPauseTime = .5;								//1.0	
+		double firstForwardTime = 2.75 + startPauseTime;		//2.0
+		double firstTurnTime = .625 +firstForwardTime;			//2.5
+		double secondForwardTime = .5 + firstTurnTime;			//3.5
+		double dipTime = 0.6 + secondForwardTime;				//7.0
+		double lastTime = 6.0;
+
+		if(timeB < startPauseTime){
+		}else if (timeB < firstForwardTime) {
 			hkdrive.goForwardVoltage(forwardVoltage);
-			//liftMani.elevatorLift(liftMani.switchHeight);		//bring the cube "up"
+			liftMani.elevatorLift(liftMani.switchHeight);	
+		}else if(timeB < firstTurnTime) {
+			hkdrive.turnVoltage(turnVoltage);
+		}else if(timeB < secondForwardTime) {
+			hkdrive.goForwardVoltage(forwardVoltage);
 		}
 
 		else if((currentPosition == 1 && getAngleToSwitchPlate() == -90
 				|| currentPosition == 3 && getAngleToSwitchPlate() == 90 )
-				&& timeA<lastTime) { 
+				&& timeB<lastTime) { 
 
-			if (timeA < dipTime){
+			if (timeB < dipTime){
 				hkdrive.stop();										//stop driving
 				liftMani.dipMouth();								//let the mouth "Dip"
-			}else if(timeA < lastTime){
+			}else if(timeB < lastTime){
 				mouthMani.spit();									//spit out powercube
 			} 
 		}
@@ -214,50 +216,40 @@ public class Robot<m_robotDrive> extends IterativeRobot {
 
 	}
 
-
-	//AUTO STRATEGY #2: Go forward to a particular plate of our switch, drop off the powercube if its the correct one -Marlahna
-	public void simpleSwitchStrategy(double timeB, int position) {
-
-	}
-
-	//AUTO STRATEGY #3: Staring in position 2, move to the correct switch plate and drop off the powercube -Marlahna
+	//AUTO STRATEGY #3: Starting in position 2, move to the correct switch plate and drop off the powercube -Marlahna
 	//Rickeya,Jocelyn,Mohamed C 
 	public void smartSwitchStrategy(double timeC) {
 
-		//double forwardSpeed = 0.5;
 		double forwardVoltage = 6.0;
 		double turnVoltage = 8.0;
 
-
+		//check which direction to go based on plate color
 		if(this.getAngleToSwitchPlate() == -90) {
 			turnVoltage *= -1;
 		}
 
-		double startPauseTime = .5;						//1.0	
-		double firstForwardTime = 0.5 + startPauseTime;			//2.0
-		double firstTurnTime = .625 +firstForwardTime;			//2.5
-		double secondForwardTime = 1.1250 + firstTurnTime;			//3.5
-		double secondTurnTime = 0.625 + secondForwardTime;		//4.0
-		double forwardToSwitchTime = 2.0 + secondTurnTime;		//6.0
-		double dipTime = 0.3 + forwardToSwitchTime;				//7.0
+		double startPauseTime = 0.5;							//0.5	
+		double firstForwardTime = 0.5 + startPauseTime;			//1.0
+		double firstTurnTime = 0.625 +firstForwardTime;			//1.625
+		double secondForwardTime = 1.1250 + firstTurnTime;		//2.75
+		double secondTurnTime = 0.625 + secondForwardTime;		//3.375
+		double forwardToSwitchTime = 2.0 + secondTurnTime;		//5.575
+		double dipTime = 0.3 + forwardToSwitchTime;				//5.875
 		double lastTime = 8.0;
 
 		if(timeC < startPauseTime){
 		}else if (timeC < firstForwardTime) {
 			hkdrive.goForwardVoltage(forwardVoltage);
-			//hkdrive.goForwardPercentOutput(forwardSpeed);
 		}else if(timeC < firstTurnTime) {
 			hkdrive.turnVoltage(turnVoltage);
 			//hkdrive.autoTurn(angleToOurSwitchPlate);		
 		}else if(timeC < secondForwardTime) {
 			hkdrive.goForwardVoltage(forwardVoltage);
-			//hkdrive.goForwardPercentOutput(forwardSpeed);
 		}else if(timeC < secondTurnTime) {
 			hkdrive.turnVoltage(-turnVoltage);
 			//hkdrive.autoTurn(-angleToOurSwitchPlate);
 		}else if(timeC < forwardToSwitchTime) {
 			hkdrive.goForwardVoltage(forwardVoltage);
-			//hkdrive.goForwardPercentOutput(forwardSpeed);
 			liftMani.elevatorLift(liftMani.switchHeight);		//bring the cube "up"
 		}else if (timeC < dipTime){
 			hkdrive.stop();										//stop driving
@@ -324,7 +316,7 @@ public class Robot<m_robotDrive> extends IterativeRobot {
 	}
 
 	//AUTO STRATEGY #5: Go forward to the scale and drop off a cube if its the correct plate
-	public void simpleScaleStrategy(double timeE, int position) {
+	public void simpleScaleStrategy(double timeE) {
 
 
 	}
